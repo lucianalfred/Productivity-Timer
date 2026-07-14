2c9f1acaff8c6a300f74e9dd0cd257da7f850467
Lucian Alfred <lucianalfred60@gmail.com>
2026-07-14 14:11:07 +0100

    settings page

---
+++ lib/settings.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Settings')),
+      body: Container(child: Text('Hello World')),
+    );
+  }
+}
